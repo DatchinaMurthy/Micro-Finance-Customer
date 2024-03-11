4c2d29274a4ed530e45ee30e77ace4301c8a9bd4
DatchinaMurthy S <datchina.murthy77@gmail.com>
2024-03-11 15:28:55 +0530

    Customer Initial Commit

---
+++ src/main/java/com/ds/microfinance/customer/MicroFinanceCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.ds.microfinance.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroFinanceCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroFinanceCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/microfinance/customer/MicroFinanceCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.microfinance.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroFinanceCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
